76fa089c25108dc97becbaf1f6f9dcece0efb57f
joshua onwuka <contactuche02@gmail.com>
2021-06-17 09:07:19 -0700

    added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - prints the string using printf function
+ * Return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+	/*prints the strings inputed*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
+
