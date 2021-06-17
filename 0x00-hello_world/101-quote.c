04b554e960b732d92d9c1407be307b8943710e8d
joshua onwuka <contactuche02@gmail.com>
2021-06-17 12:56:17 -0700

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using fprintf to print ou phrase quote
+ * Description: prints out string witheut usng printf
+ * Return: 1 successful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
