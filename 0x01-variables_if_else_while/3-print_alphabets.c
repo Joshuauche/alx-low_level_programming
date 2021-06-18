909ca8a9debfaeff382e3f205a186e8003104abd
joshua onwuka <contactuche02@gmail.com>
2021-06-18 08:34:08 -0700

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - returns all alphabets in lowercase
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+
+	while (x <= 'z' && y <= 'Z')
+	{
+		putchar(x);
+		putchar(y);
+
+		x++;
+		y++
+	}
+	putchar('\n');
+
+	return (0);
+}
+
