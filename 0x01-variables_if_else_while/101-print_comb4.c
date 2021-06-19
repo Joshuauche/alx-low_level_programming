8c9f74fb54f0864c02a0a00712c28aa5b4bc3653
joshua onwuka <contactuche02@gmail.com>
2021-06-19 05:12:15 -0700

    made some changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,16 +7,16 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i < '8'; i++)
+	for (i = 48; i < 56; i++)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
-			for (k = j + 1; k <= '9'; k++)
+			for (k = j + 1; k <= 58; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i != '7' || j != '8' || i != '9')
+				if (i != 55 || j != 56 || i != 57)
 				{
 					putchar(',');
 					putchar(' ');
