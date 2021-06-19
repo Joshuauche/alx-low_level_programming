931f0ba92799d11efe761ad9f3da39b4fba29566
joshua onwuka <contactuche02@gmail.com>
2021-06-19 03:43:44 -0700

    added 101-print.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints out all the numbers under 1000 with a ,
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || i != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
