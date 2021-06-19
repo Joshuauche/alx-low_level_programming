7cc868731729fc989a758787951efaf43a2607cc
joshua onwuka <contactuche02@gmail.com>
2021-06-19 05:43:02 -0700

    added and made some fixes

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Description: prints all possible combinations
+ *of two two-digit numbers.
+ * Return: 0 when executed successfully
+ */
+int main(void)
+{
+	int ifirst, i, jfirst, j;
+
+	for (ifirst = 48; ifirst < 58; ifirst++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			jfirst = ifirst;
+			for (; jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
