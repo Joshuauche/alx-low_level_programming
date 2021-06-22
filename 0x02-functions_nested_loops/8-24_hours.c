16b4a65f225136433c88e0e2e1428e1b9e257761
joshua onwuka <contactuche02@gmail.com>
2021-06-22 13:51:54 -0700

    changed some typos

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 		while  (minute < 60)
 		{
 			_putchar('0' + (hour / 10));
-			_putchar('0' + (hour % 24));
+			_putchar('0' + (hour % 10));
 			_putchar(':');
 			_putchar('0' + (minute / 10));
 			_putchar('0' + (minute % 10));
