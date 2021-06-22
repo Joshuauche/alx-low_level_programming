bdd45c6ab4bbfadafa8be0576f3360dd614ed8b4
joshua onwuka <contactuche02@gmail.com>
2021-06-22 08:22:07 -0700

    made some changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,13 +11,11 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c >= 'z')
-	{
-		_putchar('1');
-	}
-	else
-	{
-		_putchar('0');
-	}
-	return c;
+	int i;
+
+	i = c;
+	i = _islower(c);
+	_putchar(r + '0');
+	_putchar('\n');
+
 }
