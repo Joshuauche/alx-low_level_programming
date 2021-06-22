76bb29c84816f1378c2d1ac77718e2bf5e187522
joshua onwuka <contactuche02@gmail.com>
2021-06-22 09:41:28 -0700

    added 5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints +1, 0, -1
+ * @n: only operand
+ * Return: 1 as positive, 0 as zero and -1 as negative
+ */
+int print_sign(int n)
+{
+	if (n >= 49 && n <= 57)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 48)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
+
