31e1c4fe574221121ff3dc3566e8eefd5e38d0a4
joshua onwuka <contactuche02@gmail.com>
2021-06-23 11:30:48 -0700

    addeholberton.h and 0-main.c files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints positve or nagetive function
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_negative(i);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+/*multiple mains*/
+postitive_or_negative();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
