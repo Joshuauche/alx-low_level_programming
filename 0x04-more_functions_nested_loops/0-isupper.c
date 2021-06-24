57d06e15c453c1673a4b1df644139063c836a147
joshua onwuka <contactuche02@gmail.com>
2021-06-24 01:21:37 -0700

    added holberton.h file and 0-isipper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isupper - to check if value is uppercase
+ * @c: only parameter
+ * Return: success
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+	return (c);
+}
+
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*isuper*/
+int _isupper(int c);
+
+
+
+
+
+
+#endif
