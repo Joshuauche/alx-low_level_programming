5668b1a18612c924ddcaf7d2d40619de9ab48450
joshua onwuka <contactuche02@gmail.com>
2021-06-24 07:10:53 -0700

    made changess

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,11 +15,11 @@ void more_numbers(void)
 		{
 			if (i / 10 > 0)
 			{
-				_putchar((i / 10) + '0');
+				_putchar(i / 10 + '0');
 			}
 			else
 			{
-				_putchar((i % 10) + '0');
+				_putchar(i % 10 + '0');
 			}
 		}
 		_putchar('\n');
