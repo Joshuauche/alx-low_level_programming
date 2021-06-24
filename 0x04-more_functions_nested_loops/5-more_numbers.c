45c7390206f2354c2dd87b88b3829d2e6a1d7b99
joshua onwuka <contactuche02@gmail.com>
2021-06-24 06:59:47 -0700

    added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * Return: (0) successful
+ */
+void more_numbers(void)
+{
+	int count;
+	char i;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if ((i / 10) > 0)
+			{
+				_putchar((i / 10) + '0');
+			}
+			else
+			{
+				_putchar((i % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
