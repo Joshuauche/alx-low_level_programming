5a3a694ef065e284725497511361942dc5136b26
joshua onwuka <contactuche02@gmail.com>
2021-06-28 12:41:44 -0700

    added 4-print_rev.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
-
+/**
+ * print_rev - prints string in reverse direction
+ * @s: only parameter
+ */
 void print_rev(char *s)
 {
+	int i, j, length;
 
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	length = i;
+
+	for (j = length - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+		_putchar('\n');
+	}
 }
+
