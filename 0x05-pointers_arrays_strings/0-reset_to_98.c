3d26b69a7f1cdc632a436625b6f63758f72921f6
joshua onwuka <contactuche02@gmail.com>
2021-06-27 23:05:23 -0700

    added holberton.h file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - it takes a pointer to an int parameter
+ * @*n: only parameter
+ */
+
+void reset_to_98(int *n)
+{
+	int c;
+	c = *n;
+}
+
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+/*declaring putchar*/
+int _putchar(char c);
+
+/*0 reset_to_98*/
+int _putchar(char c);
