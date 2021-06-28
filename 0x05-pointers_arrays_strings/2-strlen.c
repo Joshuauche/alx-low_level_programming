b015ba9602e6f09796af54e564ec4b3df924fa27
joshua onwuka <contactuche02@gmail.com>
2021-06-28 01:45:07 -0700

    added 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - this return the string length of varaibles
+ * @s: only parameter
+ */
+int _strlen(char *s)
+{
+	return (strlen(c));
+}
+
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -8,8 +8,11 @@ int _putchar(char c);
 /*0 reset_to_98*/
 int _putchar(char c);
 
-/*swap pointer*/
+/*1 swap pointer*/
 void swap_int(int *a, int *b);
+
+/*2 string length*/
+int _strlen(char *s);
 
 
 
