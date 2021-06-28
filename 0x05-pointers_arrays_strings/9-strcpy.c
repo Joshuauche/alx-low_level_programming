78c888641c5634633a65445e3d0c5aafb3eaae7b
joshua onwuka <contactuche02@gmail.com>
2021-06-28 13:05:51 -0700

    added 9-strcpy.c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string to another string variable
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: a string copy when executed
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length, i
+
+	length = strlen(src);
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
