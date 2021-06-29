064dda14daacfb4c3e297fa42afa64bfcb80012c
joshua onwuka <contactuche02@gmail.com>
2021-06-28 23:08:55 -0700

    fixed pointer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,20 +9,19 @@ int _atoi(char *s)
 {
 	unsigned int num = 0;
 	int sign = 1;
-	char j = *s;
 
 	do {
-		if (j == '-')
+		if (*s == '-')
 			sign *= 1;
-		else if (j >= '0' && j <= '9')
+		else if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (j - '0');
+			num = (num * 10) + (*s - '0');
 		}
 		else if (num > 0)
 		{
 			break;
 		}
-	} while (j++);
+	} while (*s++);
 
 	return (num * sign);
 }
