554d147bd6f3987681ab6da3ba073fb675ed39e6
joshua onwuka <contactuche02@gmail.com>
2021-07-01 06:07:51 -0700

    fixed sign error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,9 +23,9 @@ char *cap_string(char *s)
 			{
 				if (s[i] == spe[j])
 				{
-					if (s[i - 1] >= 'a' && s[i - 1] <= 'z')
+					if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 					{
-						s[i - 1] -= 32;
+						s[i + 1] -= 32;
 					}
 
 				}
