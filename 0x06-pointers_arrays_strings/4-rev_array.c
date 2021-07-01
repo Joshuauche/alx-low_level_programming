3efad1d129d99ec3f97fb354f23b988a706ae016
joshua onwuka <contactuche02@gmail.com>
2021-06-30 23:37:53 -0700

    added 4-rev_array.c file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 #include <string.h>
 #include <stdio.h>
-
+/**
+ * reverse_array - this reverses the element in an array
+ * @a: array
+ * @n: size of the array
+ */
 void reverse_array(int *a, int n)
 {
+	int index;
+	int temp;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		temp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = temp;
+	}
+}
+
