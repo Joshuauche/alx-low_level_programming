ea92eca43b388621d5a535cb08f49c5bf962c3e3
joshua onwuka <contactuche02@gmail.com>
2021-07-05 03:37:46 -0700

    added 0-memset.c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills the memory with a constant byte
+ * @s: pointer parameter
+ * @b: character parameter
+ * @n: size parameter
+ * Return: the char parameter when executed
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *_memset(char *s, char b, unsigned int n)
+	{
+		int i;
+		
+		i = 0;
+		while (s && n > 0)
+		{
+			*s = b;
+			*s++;
+			n--;
+		}
+		return (s);
+	}
+}
+
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -3,7 +3,8 @@
 
 int _putchar(char c);
 
-
+/*0 files memory*/
+char *_memset(char *s, char b, unsigned int n);
 
 
 
