ed5bdfba51438aa02c29951fcb8dce82cbe46ad0
joshua onwuka <contactuche02@gmail.com>
2021-07-07 06:39:55 -0700

    fixed errors

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,6 +13,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
 	else if (y % 2 == 0)
 	{
 		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
