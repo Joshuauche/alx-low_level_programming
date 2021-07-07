34970d3ceb77bb5c401e055bc06cb2afc7cbeade
joshua onwuka <contactuche02@gmail.com>
2021-07-07 04:14:37 -0700

    added 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints the whole string
+ * @s: string parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/*0 print a string*/
+void _puts_recursion(char *s);
+
+
+
+
+
+
+
+
+
+
+
+#endif
