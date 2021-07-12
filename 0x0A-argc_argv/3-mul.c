ff08fb6e74c7e6974d0fa5139e2cda3f396734e2
joshua onwuka <contactuche02@gmail.com>
2021-07-12 05:35:29 -0700

    added 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of arguments
+ * @argc: argc parameter
+ * @argv: argv parameter
+ *
+ * Return: the product of all argments
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("error");
+		return (1);
+	}
+
+
+	return (0);
+}
+
