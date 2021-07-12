b6b6f74591581ea0d059239d4e6f51f1cfd78dea
joshua onwuka <contactuche02@gmail.com>
2021-07-12 05:46:30 -0700

    added 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the sum of arguments
+ * @argc: argc parameter
+ * @argv: argv parameter
+ *
+ * Return: prints the sum of all arguments
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (*argv[i] < 48 || *argv[i] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = sum + atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		putchar('0');
+		putchar('\n');
+	}
+
+	return (0);
+}
+
