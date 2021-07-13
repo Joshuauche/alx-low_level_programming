248f08deb4f10ed598957f13c0ac6fa45c184efb
joshua onwuka <contactuche02@gmail.com>
2021-07-13 08:45:04 -0700

    fixed errors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,44 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *result = malloc(strlen(s1) + strlen(s2) + 1);
+	char *result;
+	int i, j, length1, length2;
 
-	strcpy(result, s1);
-	strcat(result, s2);
+	i = 0, j = 0;
 
+	while (s1 && s1[length1])
+	{
+		length1++;
+	}
+	while (s2 && s2[length2])
+	{
+		length2++;
+	}
+
+	result = malloc(sizeof(char) * (length1 + length2 + 1));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	if (s1)
+	{
+		while (i < length1)
+		{
+			result[i] = s1[i];
+		}
+
+	}
+	if (s2)
+	{
+		while (i < (length1 + length2))
+		{
+			result[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	result[i] = '\0';
 	return (result);
 }
 
