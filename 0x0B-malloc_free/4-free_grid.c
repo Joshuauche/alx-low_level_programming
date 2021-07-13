d60adbbb3f90aa4f2e0e84ba157cf6f46ad8af45
joshua onwuka <contactuche02@gmail.com>
2021-07-13 10:23:31 -0700

    fixed errors

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - frees the memomory of 2 dimensional array
+ * @grid: grid parameter
+ * @height: height parameter
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
@@ -15,6 +20,7 @@ void free_grid(int **grid, int height)
 	{
 		free(grid[i]);
 	}
+	free(grid);
 
 }
 
