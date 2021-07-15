25bcb042dc31f4ef9d8dc41963dd2ae04f5fe13c
joshua onwuka <contactuche02@gmail.com>
2021-07-15 03:33:23 -0700

    added 1-string_nconcat.c file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,6 +6,9 @@ int _putchar(char c);
 /*0. allocate memory using malloc*/
 void *malloc_checked(unsigned int b);
 
+/*1. concatenates two strings*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 
 
 
