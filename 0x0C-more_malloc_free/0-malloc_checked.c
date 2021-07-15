9e1f31136c3bf16ef2a7956e4102261b145cefc3
joshua onwuka <contactuche02@gmail.com>
2021-07-15 09:56:08 -0700

    checked error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,12 +11,11 @@ void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
-	ptr = (int *)malloc(sizeof(int) * b);
+	ptr = (int *) malloc(sizeof(int) * b);
 
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
 }
 
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -12,6 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 /*2. allocate memory for an array*/
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/*3. create an array of integers*/
+int *array_range(int min, int max);
+
 
 
 
