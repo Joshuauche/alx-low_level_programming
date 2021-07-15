1c5acb92dbf62152f2a7183642db4b51fc2e1b74
joshua onwuka <contactuche02@gmail.com>
2021-07-15 00:37:58 -0700

    created 0x0c-more_malloc_free dir, header file, readme file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+void_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(b));
+}
+
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*0. allocate memory using malloc*/
+void *malloc_checked(unsigned int b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
