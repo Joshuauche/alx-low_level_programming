9a4f92cbed8526c58ed7bad07745af1f21610e35
joshua onwuka <contactuche02@gmail.com>
2021-07-15 08:43:09 -0700

    fixed betty errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * malloc_checked - allocates space
  * @b: int parameter
+ * Return: the pointer
  */
 void *malloc_checked(unsigned int b)
 {
