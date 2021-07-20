63ea61e08dc89b2d5b24ee8f9f980f89928e7e2f
joshua onwuka <contactuche02@gmail.com>
2021-07-19 22:53:47 -0700

    fixed it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,4 +23,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
+
+char *_strcpy(char *dest, char *str);
+
+int _strlen(char *s);
 #endif
