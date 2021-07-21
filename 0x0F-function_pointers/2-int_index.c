547865e7f8f1795c8f309f1da711edb3e1db6431
joshua onwuka <contactuche02@gmail.com>
2021-07-21 00:17:51 -0700

    added 2-int_index.c file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array parameter
+ * @size: size parameter
+ * @cmp: function pointer parameter
+ * Return: the index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (1);
+		}
+	}
+	return (-1);
+}
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,7 +10,8 @@ void print_name(char *name, void (*f)(char *));
 /*1.*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-
+/*2.*/
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 
