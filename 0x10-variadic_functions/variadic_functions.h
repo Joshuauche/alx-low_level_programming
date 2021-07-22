625f798a1c7b0aaf2e10911291e677e1ab2e7176
joshua onwuka <contactuche02@gmail.com>
2021-07-22 05:58:39 -0700

    added fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,8 @@
 /*0. returns the sum of all its parameters*/
 int sum_them_all(const unsigned int n, ...);
 
+/*1. print the numbers with separator*/
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 
 
