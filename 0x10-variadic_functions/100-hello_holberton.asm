ed80085b55af6a6bb1545bf62fe719bca3669c2c
joshua onwuka <contactuche02@gmail.com>
2021-07-22 13:09:32 -0700

    fixed

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,25 @@
+ Hello, Holberton to the console using system calls. Runs on 64-bit Linux
+; To assemble and run:
+;
+;      nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello
+; -----------------------------------------------------------------------------------
 
-	  global    _start
+section .text
+global main
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+main:
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 32
+	mov	rax, 1
+	mov	rdi, 1
+	mov 	rsi, message
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
 
-          section   .data
-message:  db        "Hello, Holberton", 14      ; note the newline at the end
+	section .data
+message: db	"Hello, Holberton", 10
+
