90b8cae9c31eeb8f78bfa8e432cb2fe24e901b19
joshua onwuka <contactuche02@gmail.com>
2021-07-22 03:48:22 -0700

    added file and header file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,15 @@
+#ifndef 3_CALC_H
+#define 3_CALC_H
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds two intgers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: addition of two integers
+ */
+int op_add(int a, int int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two intgers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: subtraction of two integers
+ */
+int op_sub(int a, int int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - divides two intgers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: division of two integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus two intgers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: modulus of two integers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
