aec7afb5dde9c20aedd22fec787ac3dde1421dd7
joshua onwuka <contactuche02@gmail.com>
2021-07-29 05:40:23 -0700

    new file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements
+ * @h: header node parameter
+ *
+ * Return: counter
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp = h;
+	unsigned int counter = 0;
+
+	while (tmp != NULL)
+	{
+		if (tmp->next == NULL)
+		{
+			printf("[%d] ", tmp->len);
+			printf("%s \n", tmp->str);
+		}
+		else
+		{
+			if (tmp->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%d] ", tmp->len);
+				printf("%s \n", tmp->str);
+			}
+		}
+		counter += 1;
+		tmp = tmp->next;
+	}
+	return (counter);
+}
+
+
