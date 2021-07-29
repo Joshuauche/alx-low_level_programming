683e063a3723668754271fa703ae4a5f56648365
joshua onwuka <contactuche02@gmail.com>
2021-07-29 13:40:05 -0700

    modified

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_string - executes before the main function
+ */
+void print_string(void) __attribute__ ((constructor));
+
+void print_string(void)                   
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\\n\n");
+}
+
