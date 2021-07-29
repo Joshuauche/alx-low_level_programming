d19366ed19650503c8ebc8331d6d5dbe460c32d5
joshua onwuka <contactuche02@gmail.com>
2021-07-29 13:50:33 -0700

    modified

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,9 @@
  */
 void print_string(void) __attribute__ ((constructor));
 
-void print_string(void)                   
+void print_string(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\\n\n");
+	printf("You're beat! and yet, you must allow,\nI bore");
+	printf(" my house upon my back!\\n\n");
 }
 
