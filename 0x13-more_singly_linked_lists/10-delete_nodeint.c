a64321236a93af31aa79ecba772c4be6942b7919
joshua onwuka <contactuche02@gmail.com>
2021-08-03 20:01:16 -0700

    modified

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < index - 1; i++)
 	{
-		if (current->next == NULL)
+		if (!current || current->next == NULL)
 		{
 			return (-1);
 		}
