b3c28a2aab19c7233f2d8e8ebbaab2265894beb8
joshua onwuka <contactuche02@gmail.com>
2021-08-05 09:54:29 -0700

    modified

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - prints the binary representation of a number
+ * @b: b parameter
+ *
+ * Return: res
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int n;
+	unsigned int res = 0;
+
+	if (!b)
+		return (0);
+
+	for (n = 0; *(b + n); n++)
+	{
+		if (*(b + n) < '0' || *(b + n) > '1')
+			return (0);
+		res = 2 * res + (*(b + n) - '0');
+	}
+	return (res);
+}
+
