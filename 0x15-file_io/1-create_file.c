4680659f5c73a1bc4c4c224f76b35dcf9e884bd0
joshua onwuka <contactuche02@gmail.com>
2021-08-09 08:29:17 -0700

    added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+
+/**
+ * create_file - function that creates a file
+ * @filename: filename parameter
+ * @text_content: textcontent parameter
+ *
+ * Return: 1 if successful
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd_open, fd_write, count;
+
+
+	count = 0;
+	fd_open = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+
+
+	if (fd_open < 0)
+	{
+		return (0);
+	}
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+
+	if (text_content)
+	{
+		while (*(text_content + count))
+		{
+			count++;
+		}
+		fd_write = write(fd_open, text_content, count);
+
+		if (fd_write != count)
+		{
+			return (-1);
+		}
+	}
+
+	close(fd_open);
+	return (1);
+}
+
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -14,6 +14,10 @@
 /*0. function that reads a text file and prints it to the POSIX.*/
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/*1. creates a fuction that creates a file*/
+int create_file(const char *filename, char *text_content);
+
+
 
 #endif
 
