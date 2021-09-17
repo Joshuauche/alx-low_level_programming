d292a01e127af33e16e594a7dd631d6b57bc79e6
joshua onwuka <contactuche02@gmail.com>
2021-09-17 03:35:46 -0700

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,9 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *h;
-		*h = new;
-		return (new);
+		return (add_dnodeint_end(h, n));
 	}
 	for (i = 0; temp && i < idx; i++)
 	{
