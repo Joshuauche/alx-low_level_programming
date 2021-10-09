d70021b3b9da4db9a397cb5a2a4f9dda0b3dfbb4
joshua onwuka <contactuche02@gmail.com>
2021-10-09 15:42:32 -0700

    updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -54,4 +54,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 /*5 - prints a hash table*/
 void hash_table_print(const hash_table_t *ht);
+
+/*6 - deletes a hash table*/
+void hash_table_delete(hash_table_t *ht);
 #endif
