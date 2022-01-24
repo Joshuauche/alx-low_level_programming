e37682fea83fb2b1bee927b5eeab151e026c80b1
joshua uche <contactuche02@gmail.com>
2022-01-24 14:36:17 +0000

    added more functions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
 
 
 
@@ -13,12 +16,4 @@ int linear_search(int *array, size_t size, int value);
 
 
 
-
-
-
-
-
-
-
-
-#endif 
+#endif
